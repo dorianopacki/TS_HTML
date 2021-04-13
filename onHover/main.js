9eb072e6900ab0b93471e69f67e6467c97dd50ef
Dorian Opacki <dorianwew@gmail.com>
2021-04-13 20:57:11 +0200

    reduce amount of code and fix img render problem

--- onHover/main.js
+++ onHover/main.js
@@ -1,26 +1,20 @@
 var hoverElements = document.querySelectorAll(".zoomOnHover");
 var createHoverElement = function () {
-    console.log("Created element");
     var zoomElement = document.createElement("div");
     zoomElement.classList.add("zoomElement");
     document.body.appendChild(zoomElement);
-    //to set in styles as class or whatever
     zoomElement.style.backgroundImage = "url(./cm997hae_2.png)";
-    zoomElement.style.backgroundRepeat = "no-repeat";
-    zoomElement.style.backgroundSize = "200%";
-    //dynamicly should be changing depending on x and y axis move over image
 };
 var removeHoverElement = function () {
-    console.log("Removed element");
     document.body.querySelector(".zoomElement").remove();
 };
 var moveOverHoverElement = function (event) {
-    var x = event.clientX;
-    var y = event.clientY;
     var elm = document.querySelector(".zoomElement");
+    var rect = event.target.getBoundingClientRect();
+    var x = ((event.clientX - rect.left) / rect.width) * 100;
+    var y = ((event.clientY - rect.top) / rect.height) * 100;
     elm.style.backgroundPosition = x + "% " + y + "%";
 };
-// Listening on events
 hoverElements.forEach(function (element) {
     return element.addEventListener("mouseover", createHoverElement);
 });
--- onHover/main.ts
+++ onHover/main.ts
@@ -1,38 +1,30 @@
 const hoverElements = document.querySelectorAll(".zoomOnHover");
 
 const createHoverElement = () => {
-  console.log("Created element");
   const zoomElement = document.createElement("div");
   zoomElement.classList.add("zoomElement");
   document.body.appendChild(zoomElement);
-  //to set in styles as class or whatever
   zoomElement.style.backgroundImage = "url(./cm997hae_2.png)";
-  zoomElement.style.backgroundRepeat = "no-repeat";
-  zoomElement.style.backgroundSize = "200%";
-  //dynamicly should be changing depending on x and y axis move over image
 };
 
 const removeHoverElement = () => {
-  console.log("Removed element");
   document.body.querySelector(".zoomElement").remove();
 };
 
 const moveOverHoverElement = (event: any) => {
-  let x = event.clientX;
-  let y = event.clientY;
-
   const elm = document.querySelector<HTMLElement>(".zoomElement")!;
+  const rect = event.target.getBoundingClientRect();
+  const x = ((event.clientX - rect.left) / rect.width) * 100;
+  const y = ((event.clientY - rect.top) / rect.height) * 100;
   elm.style.backgroundPosition = `${x}% ${y}%`;
 };
 
-// Listening on events
 hoverElements.forEach((element) =>
   element.addEventListener("mouseover", createHoverElement)
 );
 hoverElements.forEach((element) =>
   element.addEventListener("mouseout", removeHoverElement)
 );
-
 hoverElements.forEach((element) =>
   element.addEventListener("mousemove", moveOverHoverElement)
 );
