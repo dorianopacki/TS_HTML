df682d1d06f91d58b183fc2ef9121676fe536a33
Dorian Opacki <dorian.opacki@sollers.eu>
2021-05-24 19:04:53 +0200

    add method to render progress bars

--- progressReadingBar/main.ts
+++ progressReadingBar/main.ts
@@ -1,21 +1,35 @@
-class ProgressReadingBar {
-  element: string;
+export class ProgressReadingBarObserver {
+  htmlElements: HTMLElement[]; 
 
-  constructor(element: string) {
-    this.element = element;
+  constructor(...htmlElements: HTMLElement[]) {
+    this.htmlElements = htmlElements;
   }
 
-  createProgressBar(): void {
-    const elementToObserve = document.querySelector(`.${this.element}`);
-
+  initProgressBar() {
     const observer = new IntersectionObserver((entries) => {
-      console.log(entries[0].target);
-    });
+      entries.forEach((entry) => {
+        entry.target.classList.add("with-progress-reading-bar");
+        const progressBarWrapper = document.createElement("div");
+        const progressBarInside = document.createElement("div");
+    
+        function scroll() {
+          const winScroll = document.documentElement.scrollTop - entry.target.offsetTop + document.documentElement.clientHeight;
+          const height = entry.boundingClientRect.height;
+          const scrolled = (winScroll / height) * 100;
+          progressBarInside.style.width = scrolled + "%";
+        }
+  
+        window.addEventListener("scroll", scroll);
+    
+        entry.target.insertAdjacentElement("afterbegin", progressBarWrapper);
+        progressBarWrapper.classList.toggle("progress-bar-wrapper");
+        progressBarWrapper.appendChild(progressBarInside);
+        progressBarInside.classList.toggle("progress-bar-inside");
 
-    observer.observe(elementToObserve);
+        observer.unobserve(entry.target);
+      });
+    });
+ 
+  this.htmlElements.forEach((element) => observer.observe(element));
   }
 }
-
-const progressbar = new ProgressReadingBar("with-progress-bar");
-
-progressbar.createProgressBar();
